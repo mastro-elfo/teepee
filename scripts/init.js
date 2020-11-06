6834c84062835456d029efb24650bae1ed31b924
mastro-elfo <francesco.209@gmail.com>
2020-11-06 09:34:52 +0100

    Comments

--- scripts/init.js
+++ scripts/init.js
@@ -1,13 +1,20 @@
-const { version } = require("../package.json");
+// Initialize file ./src/version.json
+
 const { writeFileSync } = require("fs");
 
+// Get version from package
+const { version } = require("../package.json");
+
+// Explode version into maj, min, and build
 const [_, maj, min, build] = version.match(/(\d+)\.(\d+)\.(\d+)/);
+
+// Write syncronously to file
 writeFileSync(
   "./src/version.json",
   JSON.stringify({
     version,
     maj,
     min,
-    build
+    build,
   })
 );

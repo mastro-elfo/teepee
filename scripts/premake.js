17e78370c477b1a8e4cda11c35d4b651f255616e
mastro-elfo <francesco.209@gmail.com>
2020-11-08 11:52:10 +0100

    Add premake script

---
+++ scripts/premake.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process");
+
+// Check if src/i18n.js has `lng` option commented
+
+exec(
+  'cat ./src/i18n.js | grep "lng:" | cut -d "/" -f 1',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+    if (stderr) {
+      console.error(stderr);
+      process.exit(2);
+    }
+    stdout = stdout.trim();
+    if (stdout === "") {
+      return;
+    }
+    if (!stdout.match(/\s*\/\/\s*lng/)) {
+      console.error("Option lng in src/i18n.js must be commented");
+    }
+  }
+);

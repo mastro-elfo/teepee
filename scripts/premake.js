23f6283feceaf11ec73b2c809a3705783675c1b0
mastro-elfo <francesco.209@gmail.com>
2020-11-11 12:02:08 +0100

    Check changelog for current version

--- scripts/premake.js
+++ scripts/premake.js
@@ -1,7 +1,10 @@
 const { exec } = require("child_process");
+const path = require("path");
+
+// Get version and makers from package
+const { version } = require("../package.json");
 
 // Check if src/i18n.js has `lng` option commented
-
 exec(
   'cat ./src/utils/i18n.js | grep "lng:" | cut -d "/" -f 1',
   (error, stdout, stderr) => {
@@ -22,3 +25,21 @@ exec(
     }
   }
 );
+
+// Check if changelog(s) have an item about the actual version
+["en", "it"].forEach((lng) => {
+  const filename = path.join("..", "src", "locales", lng, "Changelog.json");
+  const { Changelog } = require(filename);
+  const log = Changelog.find((item) => item.version === version);
+  if (!log) {
+    console.error(
+      `Changelog not found for version ${version} in language ${lng}`
+    );
+    process.exit(3);
+  }
+  const date = new Date(log.date);
+  if (!(date instanceof Date) || isNaN(date)) {
+    console.error(`Changelog has invalid date: ${log.date}`);
+    process.exit(4);
+  }
+});

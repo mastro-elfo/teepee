89abd5177dfa43d87952d00217d529ec7124f04d
mastro-elfo <francesco.209@gmail.com>
2020-11-22 10:36:30 +0100

    Remove local changelog

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,9 +1,7 @@
 import defaultNS from "./defaultNS.json";
 import HelpContent from "./HelpContent.json";
-import Changelog from "./Changelog.json";
 const lng = {
   ...defaultNS,
-  Changelog,
   HelpContent,
 };
 export default lng;

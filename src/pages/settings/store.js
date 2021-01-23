d0ffbde870bd91d5b7f6b593b6a972bb2b71913a
mastro-elfo <francesco.209@gmail.com>
2021-01-23 16:37:20 +0100

    Remove commented code

--- src/pages/settings/store.js
+++ src/pages/settings/store.js
@@ -1,17 +1,3 @@
-// const palette = "palette";
-//
-// export function storePalette(obj) {
-//   localStorage.setItem(palette, JSON.stringify(obj));
-// }
-//
-// export function loadPalette(defaultValue) {
-//   try {
-//     return JSON.parse(localStorage.getItem(palette));
-//   } catch {
-//     return defaultValue;
-//   }
-// }
-
 export function load(key, defaultValue) {
   try {
     return JSON.parse(localStorage.getItem(key));

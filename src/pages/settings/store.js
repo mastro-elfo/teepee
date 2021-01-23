50dac01b04052a80568d39f205c762782b7ea9bb
mastro-elfo <francesco.209@gmail.com>
2021-01-23 16:25:48 +0100

    Add currency settings

--- src/pages/settings/store.js
+++ src/pages/settings/store.js
@@ -11,3 +11,25 @@
 //     return defaultValue;
 //   }
 // }
+
+export function load(key, defaultValue) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch {
+    return defaultValue;
+  }
+}
+
+export function store(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+const currency = "currency";
+
+export function loadCurrency() {
+  return load(currency, "€");
+}
+
+export function storeCurrency(value) {
+  return store(currency, value);
+}

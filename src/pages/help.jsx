63ddf1e0035b8fb62a2d90b21f874cb2271e95ec
mastro-elfo <francesco.209@gmail.com>
2021-01-24 09:18:17 +0100

    Update help page

--- src/pages/help.jsx
+++ src/pages/help.jsx
@@ -272,26 +272,27 @@ function Component() {
             handleClose={() => {}}
           />
 
-          {
-            // Settings
-            // <H1>{t("HelpContent:H1.4")}</H1>
-            // <P>
-            //   t("HelpContent:P.0")
-            //   <MenuIcon fontSize="small" />
-            //   t("HelpContent:P.1")
-            //
-            //   <Push
-            //     Component={Button}
-            //     href="/settings"
-            //     color="primary"
-            //     variant="contained"
-            //     size="small"
-            //     title={t("HelpContent:title.3")}
-            //   >
-            //     {t("HelpContent:Label.3")}
-            //   </Push>.
-            // </P>
-          }
+          <H1>{t("HelpContent:H1.4")}</H1>
+
+          <H2>{t("HelpContent:H2.6")}</H2>
+
+          <P>
+            {t("HelpContent:P.0")}
+            <MenuIcon fontSize="small" />
+            {t("HelpContent:P.1")}
+            <Push
+              Component={Button}
+              href="/settings"
+              color="primary"
+              variant="contained"
+              size="small"
+              title={t("HelpContent:title.3")}
+            >
+              {t("HelpContent:Label.3")}
+            </Push>
+            .
+          </P>
+
           <P></P>
         </Content>
       }

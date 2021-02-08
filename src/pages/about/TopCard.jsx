3855c8048cd5e61189901db4dbc7c68b7aa3a3f3
mastro-elfo <francesco.209@gmail.com>
2021-02-08 10:20:32 +0100

    Group imports

--- src/pages/about/TopCard.jsx
+++ src/pages/about/TopCard.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { remote } from "electron";
+import { remote, shell } from "electron";
 
 import LogoIcon from "../../assets/Logo";
-
-import { shell } from "electron";
 
 import {
   Avatar,
